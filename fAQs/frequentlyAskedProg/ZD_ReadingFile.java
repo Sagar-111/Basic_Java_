e18cbfcfa3a3c44bc419e535df1e7ce3da455404
Sagar-111 <sgrchdhr82@gmail.com>
2024-02-05 07:32:01 +0530

    "05022024"

--- fAQs/frequentlyAskedProg/ZD_ReadingFile.java
+++ fAQs/frequentlyAskedProg/ZD_ReadingFile.java
@@ -7,22 +7,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ZC_ReadingFile {
+public class ZD_ReadingFile {
 
 	public static void main(String[] args) throws IOException, FileNotFoundException {
 
 //		Approach_1
-//		FileReader fileToRead = new FileReader("C:\\Users\\sgrch\\OneDrive\\Desktop\\NewFile.txt");
-//
-//		BufferedReader br = new BufferedReader(fileToRead);
-//
-//		String str;
-//
-//		while ((str = br.readLine()) != null) {
-//			System.out.println(str);
-//		}
-//
-//		br.close();
+		FileReader fileToRead = new FileReader("C:\\Users\\sgrch\\OneDrive\\Desktop\\NewFile.txt");
+
+		BufferedReader br = new BufferedReader(fileToRead);
+
+		String str;
+
+		while ((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+
+		br.close();
 
 //		Approach_2
 
@@ -38,6 +38,7 @@ public class ZC_ReadingFile {
 		
 //		sc.useDelimiter("\\Z");
 //		System.out.println(sc.next());
+		sc.close();
 	}
 
 }
