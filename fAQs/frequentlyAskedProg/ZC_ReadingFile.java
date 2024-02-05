bba4c6b4e1ea319f45da6589829b6b5f1b84a065
Sagar-111 <sgrchdhr82@gmail.com>
2024-02-05 07:23:15 +0530

    "05/04/2024"

---
+++ fAQs/frequentlyAskedProg/ZC_ReadingFile.java
@@ -0,0 +1,43 @@
+package frequentlyAskedProg;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ZC_ReadingFile {
+
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+
+//		Approach_1
+//		FileReader fileToRead = new FileReader("C:\\Users\\sgrch\\OneDrive\\Desktop\\NewFile.txt");
+//
+//		BufferedReader br = new BufferedReader(fileToRead);
+//
+//		String str;
+//
+//		while ((str = br.readLine()) != null) {
+//			System.out.println(str);
+//		}
+//
+//		br.close();
+
+//		Approach_2
+
+		File file = new File("C:\\Users\\sgrch\\OneDrive\\Desktop\\NewFile.txt");
+
+		Scanner sc = new Scanner(file);
+
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		
+//		OR
+		
+//		sc.useDelimiter("\\Z");
+//		System.out.println(sc.next());
+	}
+
+}
